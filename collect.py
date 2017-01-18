5ce37c49f701ead823ff755017dc7cd26c3352b6
Kyu Park <park@kyu.co>
2017-01-18 12:23:55 -0500

    Saving in sql in a better way

--- collect.py
+++ collect.py
@@ -4,40 +4,58 @@ import sqlite3
 from operator import itemgetter
 from datetime import datetime
 
-conn = sqlite3.connect('wgc-test.db')
-c = conn.cursor()
-loc = 'BULaw_MF_Room1'
+def normalize_bssid(bssid):
+    digits = []
+    for digit in bssid.split(':'):
+        if len(digit) == 1:
+            digit = "0" + digit
+        digits.append(digit)
+    bssid=":".join(digits)
+    return bssid
 
-c.execute("DROP TABLE {}".format(loc))
-c.execute("CREATE TABLE {} (no INTEGER, bssid CHAR(17), rssi SMALLINT, ssid CHAR(32), channel SMALLINT, time TEXT)".format(loc))
+location_name = raw_input('Enter location name: ').split(' ')
+location_table = "_".join(location_name)
+
+conn = sqlite3.connect('wgdl.db')
+c = conn.cursor()
+location_table = 'BU_Starbucks'
+
+c.execute("DROP TABLE {}".format(location_table))
+c.execute("CREATE TABLE {} (timestamp char(26))".format(location_table,))
+# c.execute("CREATE TABLE routers (bssid char(17) primary key, ssid char(32), channel SMALLINT)")
+# c.execute("CREATE INDEX index ON {}".format(loc))
 
 
 airport_cmd = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s -x > airport.plist"
 
 for count in range(1, 101):
-    print "trial no %i" % count
-    time = datetime.now().strftime('%Y-%m-%d %H:%M:%f')
+    timenow = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    print "%s Scan %i" % (timenow, count)
 
     subprocess.check_output(airport_cmd, shell=True)
     pl = plistlib.readPlist('airport.plist')
 
-    routers = []
-    for router in pl:
-        digits = []
-        for digit in router['BSSID'].split(':'):
-            if len(digit) == 1:
-                digit = "0" + digit
-            digits.append(digit)
-        router['BSSID']=":".join(digits)
+    c.execute("""INSERT INTO '{}' (timestamp) VALUES (?);""".format(location_table), (timenow,))
 
-        bssid = router['BSSID']
+    for router in pl:
+        bssid = normalize_bssid(router['BSSID'])
         rssi = int(router['RSSI'])
         ssid = router['SSID_STR']
         channel = router['CHANNEL']
 
-        print bssid, rssi, ssid, channel
+        c.execute('SELECT bssid FROM routers WHERE bssid = ?', (bssid,))
+        exist = c.fetchone()
+        if exist is None:
+            c.execute('INSERT INTO routers VALUES (?, ?, ?)', (bssid, ssid, channel))
 
-        c.execute("INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?)".format(loc), (count, bssid, rssi, ssid, channel, time))
+        columns = [i[1] for i in c.execute('PRAGMA table_info({})'.format(location_table))]
+        if bssid not in columns:
+            c.execute("ALTER TABLE {} ADD COLUMN '{}' SMALLINT DEFAULT(-100);".format(location_table, bssid))
+
+        c.execute("""UPDATE {} SET '{}' = '{}'
+            WHERE timestamp = '{}'""".format(location_table, bssid, rssi, timenow))
 
         conn.commit()
+
+    print 'Number of routers: %s' % len(columns)
 conn.close()
